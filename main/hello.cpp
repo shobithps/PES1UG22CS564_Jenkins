cd0f26d2e3a13e44ad0567569d467b7f31731a50
Shobith P S <shobithps5@gmail.com>
2025-03-13 15:57:27 +0530

    third commit

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, Iam Shobith P S\n";
   cout << "Hello, Jenkins\n";
+  cout << "I love Jenkins\n";
   cout << "I have successfully built and run this  \n";
   return 0;
 }
